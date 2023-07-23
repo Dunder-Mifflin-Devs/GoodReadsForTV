77cb0d823bb817585d8fd9fb55b3583dd7bbaaf4
RobinHerzig <robin.hrzg@gmail.com>
2023-07-22 19:38:54 -0500

    Install tailwind.config and modal is working

--- good-reads-for-tv/src/App.jsx
+++ good-reads-for-tv/src/App.jsx
@@ -1,17 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Login from './Login'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import "../dist/output.css";
+import Login from "./Login";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   const [showLogin, setShowLogin] = useState(false);
-  
+
   function handleOnClose() {
-    return setShowLogin(false)
+    return setShowLogin(false);
   }
+
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalOpen(false);
+  };
 
   return (
     <>
@@ -37,10 +48,13 @@ function App() {
       </p>
 
       {/* Modal */}
-      <button onClick={() => setShowLogin(true)}>Login</button>
-      <Login onClose={handleOnClose} visible={showLogin}/>
+      <div>
+        <button onClick={() => setShowLogin(true)}>Login</button>
+      </div>
+
+      <Login onClose={handleOnClose} visible={showLogin} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- good-reads-for-tv/src/Login.jsx
+++ good-reads-for-tv/src/Login.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-export default function Login({visible, onClose}) {
+export default function Login({ visible, onClose }) {
   function handleOnClose(e) {
-    if (e.target.id === 'login') {
+    if (e.target.id === "login") {
       onClose();
     }
   }
@@ -12,31 +12,35 @@ export default function Login({visible, onClose}) {
   }
 
   return (
-    <div id="login" onClick={handleOnClose} classname="fixed inset-0 backdrop-blur-sm flex justify-center items-center">
-        <div className="bg-white p-2 rounded w-72">
-          <h1 className="font-semibold text-center text-xl text-gray-700">
-            Welcome back
-          </h1>
-          <p className="text-center text-gray-700 mb-5">Sign in</p>
+    <div
+      id="login"
+      onClick={handleOnClose}
+      className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex items-center justify-center"
+    >
+      <div className="bg-white p-2 rounded w-72">
+        <h1 className="font-semibold text-center text-xl text-gray-700">
+          GoodReadsForTV
+        </h1>
+        <p className="text-center text-gray-700 mb-5">Sign in</p>
 
-          <div className="flex flex-col">
-            <input
-              type="text"
-              className="border border-gray-700 p-2 rounded mb-5"
-              placeholder="email@example.com"
-            />
-            <input
-              type="text"
-              className="border border-gray-700 p-2 rounded mb-5"
-              placeholder="********"
-            />
-          </div>
-          <div className="text-center">
-            <button className="px-5 py-2 bg-gray-700 text-white rounded">
-              Sign in
-            </button>
-          </div>
+        <div className="flex flex-col">
+          <input
+            type="text"
+            className="border border-gray-700 p-2 rounded mb-5"
+            placeholder="email@example.com"
+          />
+          <input
+            type="text"
+            className="border border-gray-700 p-2 rounded mb-5"
+            placeholder="********"
+          />
         </div>
+        <div className="text-center">
+          <button className="px-5 py-2 bg-gray-700 text-white rounded">
+            Sign in
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
