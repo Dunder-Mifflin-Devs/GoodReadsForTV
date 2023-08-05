6a270c3bed8720bc58717d9bfe77e15ff7ed0916
David Rodriguez <93050365+DavidRod1865@users.noreply.github.com>
2023-08-05 00:37:08 -0400

    Updated tailwind and App.jsx

--- good-reads-for-tv/tailwind.config.js
+++ good-reads-for-tv/tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./Components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
