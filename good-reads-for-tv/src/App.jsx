88752e330626d87e7d7cfac9a9d9a1ddccfefb40
David Rodriguez <93050365+DavidRod1865@users.noreply.github.com>
2023-08-05 00:23:45 -0400

    Update App.jsx
    To fix the merge conflicts, App.jsx needs to be fixed to align with the main branch

--- good-reads-for-tv/src/App.jsx
+++ good-reads-for-tv/src/App.jsx
@@ -1,20 +1,7 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import "../dist/output.css";
-import Login from "./Login";
 import Header from "./Header";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const [showLogin, setShowLogin] = useState(false);
-
-  function handleOnClose() {
-    return setShowLogin(false);
-  }
-
   const [isModalOpen, setModalOpen] = useState(false);
 
   const handleOpenModal = () => {
@@ -27,27 +14,8 @@ function App() {
 
   return (
     <>
-      <div>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-
       {/* Modal */}
-      <div>
-        <button onClick={() => setShowLogin(true)}>Login</button>
-      </div>
       <Header />
-      <Login onClose={handleOnClose} visible={showLogin} />
     </>
   );
 }
