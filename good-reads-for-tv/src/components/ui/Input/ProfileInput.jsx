4a57ffae82e565f16bbeb55a6373d52a789c5b2b
RobinHerzig <robin.hrzg@gmail.com>
2023-08-15 21:02:57 -0500

    Fix runtime error

--- good-reads-for-tv/src/components/ui/Input/ProfileInput.jsx
+++ good-reads-for-tv/src/components/ui/Input/ProfileInput.jsx
@@ -1,61 +1,94 @@
-import { useState } from 'React'
+import {useState} from 'react';
 
 const ProfileInput = () => {
+  const [formData, setFormData] = useState({
+    userName: '',
+    userAddress: '',
+    userPhone: '',
+    userBirthday: '',
+    userEmail: '',
+  });
 
-    const [formData, setFormData] = useState(
-        {
-            userName: '',
-            userAddress: '',
-            userPhone: '',
-            userBirthday: '',
-            userEmail: ''
-        }
-    )
+  const handleChange = e => {
+    const {name, value} = e.target;
+    setFormData(prevData => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
 
-    const handleChange = (e) => {
-        const { name, value } = e.target
-        setFormData((prevData) => (
-            {
-                ...prevData,
-                [name]: value
-            }
-        ))
+  const handleSubmit = async event => {
+    event.preventDefault();
+    try {
+      const response = await fakeAPICall(formData);
+    } catch (err) {
+      //  Can use an alert or toast to the user
+      console.log('Error', err);
     }
-
-    const handleSubmit = async(event) => {
-        event.preventDefault()
-        try {
-            const response = await fakeAPICall(formData)
-        } catch (err) {
-            //  Can use an alert or toast to the user
-            console.log('Error', err)
-        }
-        // sending to backend
-        // toast notification
-        setFormData(
-            {
-                userName: '',
-                userAddress: '',
-                userPhone: '',
-                userBirthday: '',
-                userEmail: ''
-            }
-        )
-    }
+    // sending to backend
+    // toast notification
+    setFormData({
+      userName: '',
+      userAddress: '',
+      userPhone: '',
+      userBirthday: '',
+      userEmail: '',
+    });
+  };
 
   return (
     <>
-        <form className='flex flex-col items-center justify-around bg-[#ECECEC] gap-2 p-2' onSubmit={handleSubmit}>
-            <input className='bg-[#ffffff] text-black rounded-lg p-1 w-2/4' required type="text" placeholder="Name" name='userName' value={formData.userName} onChange={handleChange}/>
-            <input className='bg-[#ffffff] text-black rounded-lg p-1 w-2/4' required type="text" placeholder="Address" name='userAddress' value={formData.userAddress} onChange={handleChange}/>
-            <input className='bg-[#ffffff] text-black rounded-lg p-1 w-2/4' required type="tel" placeholder="Phone Number" name='userPhone' value={formData.userPhone} onChange={handleChange}/>
-            <input className='bg-[#ffffff] text-black rounded-lg p-1 w-2/4' required type="date" placeholder="Birthdate" name='userBirthday' value={formData.userBirthday} onChange={handleChange}/>
-            <input className='bg-[#ffffff] text-black rounded-lg p-1 w-2/4' required type="email" placeholder="Email" name='userEmail' value={formData.userEmail} onChange={handleChange}/>
+      <form className="flex flex-col items-center justify-around bg-[#ECECEC] gap-2 p-2" onSubmit={handleSubmit}>
+        <input
+          className="bg-[#ffffff] text-black rounded-lg p-1 w-2/4"
+          required
+          type="text"
+          placeholder="Name"
+          name="userName"
+          value={formData.userName}
+          onChange={handleChange}
+        />
+        <input
+          className="bg-[#ffffff] text-black rounded-lg p-1 w-2/4"
+          required
+          type="text"
+          placeholder="Address"
+          name="userAddress"
+          value={formData.userAddress}
+          onChange={handleChange}
+        />
+        <input
+          className="bg-[#ffffff] text-black rounded-lg p-1 w-2/4"
+          required
+          type="tel"
+          placeholder="Phone Number"
+          name="userPhone"
+          value={formData.userPhone}
+          onChange={handleChange}
+        />
+        <input
+          className="bg-[#ffffff] text-black rounded-lg p-1 w-2/4"
+          required
+          type="date"
+          placeholder="Birthdate"
+          name="userBirthday"
+          value={formData.userBirthday}
+          onChange={handleChange}
+        />
+        <input
+          className="bg-[#ffffff] text-black rounded-lg p-1 w-2/4"
+          required
+          type="email"
+          placeholder="Email"
+          name="userEmail"
+          value={formData.userEmail}
+          onChange={handleChange}
+        />
 
-            <input className='bg-[#F2A365] text-black rounded-lg p-1' disabled type="submit" value='Create Profile'/>
-        </form>
+        <input className="bg-[#F2A365] text-black rounded-lg p-1" disabled type="submit" value="Create Profile" />
+      </form>
     </>
-  )
+  );
 };
 
 export default ProfileInput;
