30454796eb11dbc0dc9c9bc4ef94e27ea0cff8c3
Mike Wawrzenczyk <mike.wawrzenczyk@gmail.com>
2023-08-31 14:54:01 -0400

    added AboutUs Page in a pages folder under src path

---
+++ good-reads-for-tv/src/pages/AboutUs.jsx
@@ -0,0 +1,22 @@
+export const AboutUs = () => {
+    return(
+        <div>
+            <h1>Who are we?</h1>
+            <p>Worth a Watch is a site dedicated to allowing people to share in their love of episodic TV shows! Wether through old media or streaming, we want people to be able to look up information on their favorite shows as well as share comment and review them.
+            </p>
+            <p>We think every show is Worth a Watch!</p>
+
+            <h1>Mission</h1>
+            <p>Help people find and share information on their favorite TV shows</p>
+
+            <h1>Services</h1>
+            <p>
+                <ul>
+                    <li>Track what shows you are watching</li>
+                    <li>Find Information on Shows</li>
+                    <li>Review shows</li>
+                </ul>
+            </p>
+        </div>
+)
+}
