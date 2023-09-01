057e85d11df5c3cf5508ddb1d838081846066ba4
RobinHerzig <robin.hrzg@gmail.com>
2023-08-31 23:19:01 -0500

    Add overflow rules for Feature title

--- good-reads-for-tv/src/components/common/Feature.jsx
+++ good-reads-for-tv/src/components/common/Feature.jsx
@@ -2,11 +2,15 @@ import featurePlaceholder from './../../assets/images/featurePlaceholder.jpg';
 
 export default function Feature({poster, title}) {
   return (
-    <div className="p-8 text-white flex flex-col">
+    <div className="p-8 text-white flex flex-col w-1/6 ">
       <div>
-        <img src={poster.img || featurePlaceholder} alt={poster.alt || 'Placeholder'} className="h-96 sharp"></img>
+        <img
+          src={poster.img || featurePlaceholder}
+          alt={poster.alt || title || 'Placeholder'}
+          className="sharp object-cover"
+        ></img>
       </div>
-      <h2 className="font-lisu text-[--white]">{title || 'Placeholder'}</h2>
+      <h2 className="font-lisu text-[--white] truncate text-ellipsis overflow-hidden">{title || 'Placeholder'}</h2>
     </div>
   );
 }
