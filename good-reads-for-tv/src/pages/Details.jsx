706e621efdcb7dc2205f723e722db51c2b503639
Jose Ruiz <joeruizl87@gmail.com>
2023-09-21 22:33:08 -0400

    adding review component

--- good-reads-for-tv/src/pages/Details.jsx
+++ good-reads-for-tv/src/pages/Details.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import Feature from './../components/common/Feature';
-
+import ReviewHolder from '../components/ui/ReviewHolder'
 const Details = () => {
   return (
     <>
       <div className="flex flex-grow flex-row flex-wrap">
         <div className="w-96">
-          <Feature poster="{{ img: '', alt: '' }}" />
+          <ReviewHolder/>
+          <Feature poster="{{ img: '', alt: '' }}" /> 
 
 
 
 
-          
+      
         </div>
       </div>
     </>
