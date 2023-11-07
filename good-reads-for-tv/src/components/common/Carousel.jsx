b1dfc3c1858901a10a702f9e863a09b1e6bf81be
RobinHerzig <robin.hrzg@gmail.com>
2023-11-06 22:35:04 -0600

    Cleaned up Carousel component and removed extra files

--- good-reads-for-tv/src/components/common/Carousel.jsx
+++ good-reads-for-tv/src/components/common/Carousel.jsx
@@ -1,77 +1,65 @@
-import React, {useCallback, useEffect, useState} from 'react';
+import React, {useCallback} from 'react';
 import useEmblaCarousel from 'embla-carousel-react';
-import {flushSync} from 'react-dom';
-import imageByIndex from '../../assets/javascript/imageByIndex';
 import '../../assets/styles/embla.css';
 import Feature from './Feature';
-import { NavLink } from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 
-const TWEEN_FACTOR = 4.2;
+const poster = '';
+const title = '';
 
-const numberWithinRange = (number, min, max) => Math.min(Math.max(number, min), max);
+export const Carousel = () => {
+  const [emblaRef, emblaApi] = useEmblaCarousel({loop: true});
 
-const Carousel = props => {
-  const {slides, options} = props;
-  const [emblaRef, emblaApi] = useEmblaCarousel(options);
-  const [tweenValues, setTweenValues] = useState([]);
+  const scrollPrev = useCallback(() => {
+    if (emblaApi) emblaApi.scrollPrev();
+  }, [emblaApi]);
 
-  const onScroll = useCallback(() => {
-    if (!emblaApi) return;
-
-    const engine = emblaApi.internalEngine();
-    const scrollProgress = emblaApi.scrollProgress();
-
-    const styles = emblaApi.scrollSnapList().map((scrollSnap, index) => {
-      let diffToTarget = scrollSnap - scrollProgress;
-
-      if (engine.options.loop) {
-        engine.slideLooper.loopPoints.forEach(loopItem => {
-          const target = loopItem.target();
-          if (index === loopItem.index && target !== 0) {
-            const sign = Math.sign(target);
-            if (sign === -1) diffToTarget = scrollSnap - (1 + scrollProgress);
-            if (sign === 1) diffToTarget = scrollSnap + (1 - scrollProgress);
-          }
-        });
-      }
-      const tweenValue = 1 - Math.abs(diffToTarget * TWEEN_FACTOR);
-      return numberWithinRange(tweenValue, 0, 1);
-    });
-    setTweenValues(styles);
-  }, [emblaApi, setTweenValues]);
-
-  useEffect(() => {
-    if (!emblaApi) return;
-
-    onScroll();
-    emblaApi.on('scroll', () => {
-      flushSync(() => onScroll());
-    });
-    emblaApi.on('reInit', onScroll);
-  }, [emblaApi, onScroll]);
+  const scrollNext = useCallback(() => {
+    if (emblaApi) emblaApi.scrollNext();
+  }, [emblaApi]);
 
   return (
     <div className="embla">
       <div className="embla__viewport" ref={emblaRef}>
         <div className="embla__container">
-          {slides.map(index => (
-            <div
-              className="embla__slide"
-              key={index}
-              style={{
-                ...(tweenValues.length && {opacity: tweenValues[index]}),
-              }}
-            >
-              <div className="embla__slide__number">
-                <span>{index + 1}</span>
-              </div>
-              <NavLink to="/details" className="w-24">
-                <Feature poster="{{ img: '', alt: '' }}" title={'TV Show 3'} />
-              </NavLink>
-            </div>
-          ))}
+          <NavLink to="/details" className="w-24 embla__slide">
+            <Feature poster={poster} title={'First'} />
+          </NavLink>
+          <NavLink to="/details" className="w-24 embla__slide">
+            <Feature poster={poster} title={'Second'} />
+          </NavLink>
+          <NavLink to="/details" className="w-24 embla__slide">
+            <Feature poster={poster} title={'Third'} />
+          </NavLink>
+          <NavLink to="/details" className="w-24 embla__slide">
+            <Feature poster={poster} title={'Fourth'} />
+          </NavLink>
+          <NavLink to="/details" className="w-24 embla__slide">
+            <Feature poster={poster} title={'Fifth'} />
+          </NavLink>
+          <NavLink to="/details" className="w-24 embla__slide">
+            <Feature poster={poster} title={'Sixth'} />
+          </NavLink>
+          <NavLink to="/details" className="w-24 embla__slide">
+            <Feature poster={poster} title={'Seventh'} />
+          </NavLink>
+          <NavLink to="/details" className="w-24 embla__slide">
+            <Feature poster={poster} title={'Eighth'} />
+          </NavLink>
+          <NavLink to="/details" className="w-24 embla__slide">
+            <Feature poster={poster} title={'Ninth'} />
+          </NavLink>
+          <NavLink to="/details" className="w-24 embla__slide">
+            <Feature poster={poster} title={'Tenth'} />
+          </NavLink>
         </div>
       </div>
+      {/* <button className="embla__prev" onClick={scrollPrev}>
+        Prev
+      </button>
+      <button className="embla__next" onClick={scrollNext}>
+        Next
+      </button> */}
     </div>
   );
 };
--- good-reads-for-tv/src/pages/dashboard.jsx
+++ good-reads-for-tv/src/pages/dashboard.jsx
@@ -1,15 +1,11 @@
 import Feature from '../components/common/Feature';
-import { NavLink } from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 import Carousel from '../components/common/Carousel';
-
-const OPTIONS = { loop: true }
-const SLIDE_COUNT = 10
-const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
 
 const Dashboard = () => {
   return (
     <section className="sandbox__carousel flex-grow">
-      <Carousel slides={SLIDES} options={OPTIONS} />
+      <Carousel />
     </section>
   );
 };
