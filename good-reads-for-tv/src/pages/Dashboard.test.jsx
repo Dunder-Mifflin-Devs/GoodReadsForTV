16ca1737a96a46001c784df829cf3a4343f69dd7
RobinHerzig <robin.hrzg@gmail.com>
2023-11-07 21:59:39 -0600

    Removed router from dashboard.jsx and placed in dashboard.test.jsx

--- good-reads-for-tv/src/pages/Dashboard.test.jsx
+++ good-reads-for-tv/src/pages/Dashboard.test.jsx
@@ -1,10 +1,15 @@
+import {BrowserRouter} from 'react-router-dom';
 import '@testing-library/jest-dom';
 import {render, screen} from '@testing-library/react';
 import Dashboard from './dashboard';
 import Feature from '../components/common/Feature';
 
 test('Dashboard renders successfully', () => {
-  render(<Dashboard />);
+  render(
+    <BrowserRouter>
+      <Dashboard />
+    </BrowserRouter>
+  );
 
   const element = screen.getByText(/TV Show 1/i);
 
