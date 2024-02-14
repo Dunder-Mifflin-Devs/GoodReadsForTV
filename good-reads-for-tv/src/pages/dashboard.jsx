fda98ce6056e7b73cec278e59811a3a876283434
Mike Wawrzenczyk <mike.wawrzenczyk@gmail.com>
2024-02-14 16:22:04 -0500

    Axios File start

--- good-reads-for-tv/src/pages/dashboard.jsx
+++ good-reads-for-tv/src/pages/dashboard.jsx
@@ -1,6 +1,8 @@
 import Carousel from '../components/common/Carousel';
+import {getRandomShows} from '../axios'
 
 const Dashboard = () => {
+  console.log(getRandomShows(20))
   return (
     <>
       <section className="sandbox__carousel flex-grow">
